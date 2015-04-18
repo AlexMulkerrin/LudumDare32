e8fc5266478a874d1f94f442e169a2b5b4d90907
Alex Mulkerrin <a.mulkerrin.12@aberdeen.ac.uk>
2015-04-18 07:34:11 +0100

    Beginning!

---
+++ src/ludumdare32/LudumDare32.java
@@ -0,0 +1,16 @@
+package ludumdare32;
+
+/**
+ *
+ * @author Alex Mulkerrin
+ */
+public class LudumDare32 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
